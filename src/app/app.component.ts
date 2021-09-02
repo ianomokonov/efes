74f090afe67541d390cb751b56808b88a62a0c0b
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2021-09-02 10:11:39 +0300

    подключил primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-  title = 'efes';
+export class AppComponent implements OnInit {
+  public title = 'efes';
+  public items: MenuItem[] = [];
+
+  public ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home' },
+      { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+      { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+      { label: 'Documentation', icon: 'pi pi-fw pi-file' },
+      { label: 'Settings', icon: 'pi pi-fw pi-cog' },
+    ];
+  }
 }
